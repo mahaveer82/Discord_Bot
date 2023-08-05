36c9ce856925759a69b6bde67ca70333fa8e6b4a
Mahaveer Jain <77223066+mahaveer82@users.noreply.github.com>
2023-08-05 18:40:45 +0530

    Update command.js

--- command.js
+++ command.js
@@ -8,14 +8,14 @@ const commands = [
   ];
 
   const rest = new REST({ version: '10' }).setToken(
-    "MTEzNzMxNTg3NDQ1ODU3ODk0NA.GePua4.NnrZXvj3bPo8_R-cb8W8FHG71HP4kKxI8ac-k4"
+    "<Your Token>"
 );
 
 (async ()=>{
 try {
     console.log('Started refreshing application (/) commands.');
   
-    await rest.put(Routes.applicationCommands('1137315874458578944'), { body: commands });
+    await rest.put(Routes.applicationCommands('<Your Client ID>'), { body: commands });
   
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
